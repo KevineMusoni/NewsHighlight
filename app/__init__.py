80a73f1529c30b1a87989c46addbe8cb103349df
Kevine Musoni <kevinenicky@gmail.com>
2018-11-12 17:08:30 +0300

    bootstrap initialised

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,19 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
-
-# initialize bootstrap
 bootstrap = Bootstrap()
 
+# bootstrap initialised
 def create_app(config_name):
 
-    #create the app instance
     app = Flask(__name__)
 
-    #set app configurations
     app.config.from_object(config_options[config_name])
 
-    #initialize bootstrap
     bootstrap.init_app(app)
-
-    #register blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    #init requests configurations
     from .requests import configure_request
     configure_request(app)
 
